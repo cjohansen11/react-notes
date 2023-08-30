e583d1de8c54baa19265aac47b024d07dd821d76
cjohansen11 <cjohansen11@me.com>
2023-08-30 16:08:38 -0400

    Added modal component for creating a new note Updated styling for note section

--- frontend/components/NoteSection/NoteSection.tsx
+++ frontend/components/NoteSection/NoteSection.tsx
@@ -1,15 +1,53 @@
-import { Search } from "..";
+import { useState } from "react";
+import { Button, Input, Search } from "..";
 import styles from "./noteSection.module.css";
+import Modal from "react-modal";
 
 export type NoteSectionProps = {
   email: string;
 };
 
 export default function NoteSection({ email }: NoteSectionProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleNewNote = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <>
-      <h1 className={styles.title}>{`${email.split("@")[0]}'s Notes`}</h1>
-      <Search />
+      <div className={styles.container}>
+        <h1 className={styles.title}>{`${email.split("@")[0]}'s Notes`}</h1>
+        <div className={styles.activityContainer}>
+          <Search />
+          <Button onClick={handleNewNote}>Create Note</Button>
+        </div>
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        shouldCloseOnEsc={true}
+        onRequestClose={() => setIsModalOpen(false)}
+        preventScroll={true}
+        className={styles.modal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+          },
+          content: {
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        }}
+      >
+        <div>
+          <p>Create a new note</p>
+          <Input placeholder="Title" />
+          <textarea placeholder="Note text" />
+          <Button>Save</Button>
+        </div>
+      </Modal>
     </>
   );
 }
